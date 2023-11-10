2a3a970689740d03ca2ad14e8349d144c4238259
Cody Mickelsen <63523816+cody-mick@users.noreply.github.com>
2023-11-10 10:53:23 -0700

    updated home page

--- client/src/routes/HomePage.tsx
+++ client/src/routes/HomePage.tsx
@@ -1,5 +1,10 @@
 const HomePage = () => {
-	return <div>Rigby Produce API</div>;
+	return (
+		<div>
+			<h1>Rigby Produce Docksides Reports</h1>
+			<p>Please make the call for a report from the Retool application</p>
+		</div>
+	);
 };
 
 export default HomePage;
